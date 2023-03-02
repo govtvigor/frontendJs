3acbc9c1dc5ab53f296cf64b62827ba963249371
marhosha23 <marhosha23@gmail.com>
2023-03-02 23:09:46 +0200

    hw22

--- js.js
+++ js.js
@@ -1,10 +1,42 @@
-let firstNumber,secondNumber, sum, division, multiply, negation;
+let firstNumber,secondNumber, sum, division, multiply, negation, question;
 
 
 firstNumber = prompt("Enter the first number: ");
 secondNumber = prompt("Enter the second number: ");
+
+
+
 sum = Number(firstNumber) + Number(secondNumber);
-division = Number(firstNumber) / Number(secondNumber);
 negation = Number(firstNumber) - Number(secondNumber);
 multiply = Number(firstNumber) * Number(secondNumber);
-alert(sum + ' ' + negation + ' ' + multiply + ' ' + division);
+division = Number(firstNumber) / Number(secondNumber);
+
+if (firstNumber === '' || secondNumber === '') {
+    alert("Error, you didnt write a number!");
+}
+
+else if (secondNumber == 0) {
+    alert("Error, you cannot divide on zero!");
+}
+
+
+ else if (Number(firstNumber) < Number(secondNumber)) {
+    question = confirm("Do u want to continue?");
+
+    if (!question) {
+        alert("You canceled.");
+       
+    }
+    else {
+        alert(negation);
+    }
+    
+    
+}
+else {
+    alert(sum + ' ' + negation + ' ' + multiply + ' ' + division);
+}
+
+
+
+
